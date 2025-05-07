8185fba01ce8a582541fe689795c62d7a5dc917c
donghch <dieshenke@gmail.com>
2025-05-07 16:19:05 -0700

    Added Inventory and Analytics Page to Router

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,11 +3,20 @@ import { createBrowserRouter } from "react-router";
 import App from "../App.jsx"
 import AdminPanel from "@/admin/AdminPanel.jsx";
 import SalePanel from "@/user/SalePanel.jsx";
+import InventoryView from "@/admin/views/InventoryView.jsx";
+import AnalyticsView from "@/admin/views/AnalyticsView.jsx";
 
 export const router = createBrowserRouter(
     [
         { path: "/", Component: App },
-        { path: "/admin", Component: AdminPanel },
+        {
+            path: "/admin",
+            Component: AdminPanel,
+            children: [
+                { path: "inventory", Component: InventoryView },
+                { path: "analytics", Component: AnalyticsView },
+            ]
+        },
         { path: "/sale", Component: SalePanel },
     ]
 );
