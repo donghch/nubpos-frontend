28e8a5fd6a7eff6e5fa63dd6f06832f9c4d1e5a8
donghch <dieshenke@gmail.com>
2025-05-07 17:19:59 -0700

    Search Popup Skeleton

--- src/components/custom/SearchPopup.jsx
+++ src/components/custom/SearchPopup.jsx
@@ -1,9 +1,25 @@
 
+import { Input } from "@chakra-ui/react";
+import Keyboard from "react-simple-keyboard";
+
+import "./SearchPopup.css";
+import "react-simple-keyboard/build/css/index.css";
+
+const keyboardLayout = {
+    default: [
+        "1 2 3",
+        "4 5 6",
+        "7 8 9",
+        "* 0 #"
+    ]
+};
+
 function SearchPopup() {
 
     return (
         <div className="search-popup">
-
+            <Input height="50px" style={{fontsize: "30px"}}/>
+            <Keyboard layout={keyboardLayout} />
         </div>
     );
 
