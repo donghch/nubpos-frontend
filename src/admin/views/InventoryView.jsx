0e6db7651abf623d56c8c293c8ebd0fbdffba4b1
donghch <dieshenke@gmail.com>
2025-05-08 17:37:50 -0700

    InventoryView.jsx with dummy table data

--- src/admin/views/InventoryView.jsx
+++ src/admin/views/InventoryView.jsx
@@ -1,16 +1,34 @@
 
 import Table from "@/components/custom/Table.jsx"
 import SearchBar from "@/components/custom/SearchBar.jsx"
+import SearchPopup from "@/components/custom/SearchPopup.jsx";
+import { Button } from "@chakra-ui/react";
 
-const tableTitles = ["Item ID", "Item Name", "Price", "Operations"]
+const tableTitles = {
+    id: "Item ID",
+    name: "Item Name",
+    price: "Price",
+    operations: "Operations"
+};
+
+const data = [
+    {
+        id: 10,
+        name: "Bomb",
+        price: 30.5,
+        operations: <Button>Shit</Button>
+    }
+];
 
 function InventoryView() {
+
 
     return (
         <>
             <p>There are more problems</p>
             <SearchBar />
-            <Table header={tableTitles} />
+            <Table header={tableTitles} data={data} />
+            <SearchPopup />
         </>
 
     );
