b8fd9b68b24770a0e9e45068e87650cd86542800
donghch <dieshenke@gmail.com>
2025-05-09 19:03:15 -0700

    Fetch inventory using axios + prep for popup

--- src/admin/views/InventoryView.jsx
+++ src/admin/views/InventoryView.jsx
@@ -2,7 +2,10 @@
 import Table from "@/components/custom/Table.jsx"
 import SearchBar from "@/components/custom/SearchBar.jsx"
 import SearchPopup from "@/components/custom/SearchPopup.jsx";
-import { Button } from "@chakra-ui/react";
+import { Button, Pagination } from "@chakra-ui/react";
+import axiosMockInstance from "@/mock/index.js";
+import "@/mock/mocks.js";
+import {useEffect, useState, useRef, useCallback} from "react";
 
 const tableTitles = {
     id: "Item ID",
@@ -20,14 +23,58 @@ const data = [
     }
 ];
 
+/* Helper Functions */
+
+const inventoryURL = "http://localhost:8888/inventory";
+
+async function fetchInventory() {
+    const res = await axiosMockInstance.get(inventoryURL);
+    if (res.status === 200)
+        return res.data;
+    else
+        throw Error(res.status);
+}
+
 function InventoryView() {
 
+    const [inventoryData, setInventoryData] = useState([]);
+    const [itemManageOpened, setItemManageOpened] = useState(false);
+    const selectedItem = useRef(null);
+
+    const manageInventoryItem = useCallback(
+        itemInfo => {
+            selectedItem.current = itemInfo;
+            setItemManageOpened(true);
+            console.log(`Current Item: ${itemInfo.name}`)
+        }, []
+    );
+
+    useEffect(
+        () => {
+            fetchInventory().then(
+                data => {
+                    data = data.map(
+                        item => {
+                            item["operations"] = (
+                                <Button key={`inventory-button-${item.id}`} onClick={() => manageInventoryItem(item)}>Manage</Button>
+                            );
+                            return item;
+                        }
+                    )
+                    setInventoryData(data);
+                },
+                err => {
+                    console.log(err);
+                }
+            )
+        }, []
+    );
 
     return (
         <>
             <p>There are more problems</p>
             <SearchBar />
-            <Table header={tableTitles} data={data} />
+            <Table header={tableTitles} data={inventoryData} />
             <SearchPopup />
         </>
 
