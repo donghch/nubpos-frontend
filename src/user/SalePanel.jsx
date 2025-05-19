827f0dcb34f689b30f0433a9a20234f8f436a52c
donghch <dieshenke@gmail.com>
2025-05-19 16:31:57 -0700

    Sale Panel Skeleton

--- src/user/SalePanel.jsx
+++ src/user/SalePanel.jsx
@@ -1,8 +1,40 @@
 
+import { HStack } from "@chakra-ui/react";
+import {useState} from "react";
+
+
+
+/* Helper Components */
+
+function ScannedItemsView({itemData}) {
+    return (
+        <p>This is scanned items</p>
+    );
+}
+
+function SaleBriefView() {
+    return (
+        <p>This is sale brief</p>
+    );
+}
+
+/* Main Component */
+
+const rootLayoutProps = {
+    height: "100vh",
+    alignItems: "top",
+    gap: "0"
+}
+
 function SalePanel() {
 
+    const [scannedItems, setScannedItems] = useState([]);
+
     return (
-        <p>This is for sale</p>
+        <HStack {...rootLayoutProps}>
+            <ScannedItemsView />
+            <SaleBriefView />
+        </HStack>
     );
 
 }
